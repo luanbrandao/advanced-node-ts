7eb28a5e2a823dfacbd7412a2c2386bcde160158
luanbrandao <luanbrandao4@gmail.com>
2023-08-03 09:54:58 -0300

    add .prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  parser: 'typescript',
+  endOfLine: 'lf',
+  importOrder: ['^[./]'],
+  importOrderSeparation: true,
+};
